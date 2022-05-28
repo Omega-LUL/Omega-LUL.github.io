12c8da596fd74acb615f02c646467237c732e4fd
Omega-LUL <73821510+Omega-LUL@users.noreply.github.com>
2022-05-28 16:55:45 +0200

    Contrast

--- Webb Slutuppgift/javascript/movingparts.js
+++ Webb Slutuppgift/javascript/movingparts.js
@@ -4,11 +4,12 @@
     let woods2 = document.getElementById('woods2')
     let title = document.getElementById('title')
 
+// Detta gör så att hemsidan rör sig när man scrollar
     window.addEventListener('scroll', function(){
       let value = window.scrollY;
       woods.style.top = value * 0.1 + 'px';
       woods1.style.top = value * 0.85 + 'px';
-
+// if satsen gör att sidan får en botten. Så om value gånger 1.25 blir mindre än 750 så stannar den.
 if(value * 1.25<750){
       woods2.style.top = value * 1.25 + 'px';
 }
