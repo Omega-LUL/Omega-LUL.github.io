09a96628664d27d6f963b13c976afe64ffb93226
Omega-LUL <73821510+Omega-LUL@users.noreply.github.com>
2022-05-29 21:18:49 +0200

    yes

--- Webb Slutuppgift/javascript/movingparts.js
+++ Webb Slutuppgift/javascript/movingparts.js
@@ -9,7 +9,7 @@
       let value = window.scrollY;
       woods.style.top = value * 0.1 + 'px';
       woods1.style.top = value * 0.85 + 'px';
-// if satsen gör att sidan får en botten. Så om value gånger 1.25 blir mindre än 750 så stannar den.
+// if satsen gör att sidan får en botten. Alltså om value * 1.25 blir mindre än 750 så stannar den.
 if(value * 1.25<750){
       woods2.style.top = value * 1.25 + 'px';
 }
